49f5a2aab431fec7fcad7006b39a0c32093e4117
Andrew <andyphiri92@gmail.com>
2024-07-28 19:14:04 +0200

    removed opencv - not needed

--- opencv/build/intermediates/cxx/Debug/3n45693s/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\andre\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\andre\\Android Studio Projects\\ColorPicker\\opencv\\.cxx\\Debug\\3n45693s\\x86_64" ^
-  opencv_jni_shared
